ebd5e18d3c3a3bd8c8efccde7590742595176f7c
Johannes Thumshirn <jthumshirn@suse.de>
2017-05-17 11:12:36 +0200

    byteorder.h: implement cpu_to_*
    Signed-off-by: Johannes Thumshirn <jthumshirn@suse.de>

--- byteorder.h
+++ byteorder.h
@@ -15,6 +15,12 @@
 #  define le64_to_cpu(x) (x)
 #  define be32_to_cpu(x) bswap_32(x)
 #  define be64_to_cpu(x) bswap_64(x)
+#  define cpu_to_le16(x) (x)
+#  define cpu_to_be16(x) bswap_16(x)
+#  define cpu_to_le32(x) (x)
+#  define cpu_to_be32(x) bswap_32(x)
+#  define cpu_to_le64(x) (x)
+#  define cpu_to_be64(x) bswap_64(x)
 #elif BYTE_ORDER == BIG_ENDIAN
 #  define le16_to_cpu(x) bswap_16(x)
 #  define be16_to_cpu(x) (x)
@@ -22,6 +28,12 @@
 #  define le64_to_cpu(x) bswap_64(x)
 #  define be32_to_cpu(x) (x)
 #  define be64_to_cpu(x) (x)
+#  define cpu_to_le16(x) bswap_16(x)
+#  define cpu_to_be16(x) (x)
+#  define cpu_to_le32(x) bswap_32(x)
+#  define cpu_to_be32(x) (x)
+#  define cpu_to_le64(x) bswap_64(x)
+#  define cpu_to_be64(x) (x)
 #else
 #  error unsupported
 #endif
